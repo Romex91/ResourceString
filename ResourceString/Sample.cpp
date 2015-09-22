ab93d8ed9cc9ace4d0b0b200aceb565edf7ee94f
Romex91 <rkuksin91@mail.ru>
2015-09-22 21:04:48 +0600

    Modified the sample to work correctly with unicode strings

--- ResourceString/Sample.cpp
+++ ResourceString/Sample.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
-#include <boost/archive/xml_oarchive.hpp>
-#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/xml_woarchive.hpp>
+#include <boost/archive/xml_wiarchive.hpp>
 
 #define PRINT_RESOURCE_STRINGS
 #include "rstring.h"
@@ -8,32 +8,34 @@ using namespace rstring;
 
 EditableResource<char, wchar_t> resource;
 
-//_rstr_t SourceCode = _rstr(R"d(
-//#include <iostream>
-//#include <string>
-//#include <regex>
-//#include"stringize.hpp" "anotherString"
-//int main()
-//{
-//	std::string lines[] = {USE_STRING_CONSTANT("Roses are #ff0000"),
-//		USE_STRING_CONSTANT("(violets are
-//							#0000ff)"),
-//							"all of my base are belong to you"
-//};
-//}
-//)d");
+//unfortunately VS build log contains extra formatting spaces on the left side
+//do not use multi-lines strings until you are going to manually delete the spaces from the log file
+_rstr_t SourceCode = _rstr(R"d(
+#include <iostream>
+#include <string>
+#include <regex>
+#include"stringize.hpp"
+int main()
+{
+	std::string lines[] = {USE_STRING_CONSTANT("Roses are #ff0000"),
+		USE_STRING_CONSTANT("(violets are
+							#0000ff)"),
+							"all of my base are belong to you"
+};
+}
+)d");
 
 
 void usageSample()
 {
-	std::ifstream xmlFs("strings.xml");
-	boost::archive::xml_iarchive archive(xmlFs, boost::archive::no_header);
+	std::wifstream xmlFs("strings.xml");
+	boost::archive::xml_wiarchive archive(xmlFs, boost::archive::no_header);
 	archive >> boost::serialization::make_nvp("strings", resource);;
 	_rstrw_t::setResource(resource);
 
 	auto string1 = _rstrw("simple string without arguments");
 	auto string2 = _rstrw("string containing some arguments : int {0}, float {1}, string {2} ", 2, 2.1f, "foo");
-	auto string3 = _rstrw("string containing another resource string : {0}", string2);
+	auto string3 = _rstrw(R"(string containing another resource string : "{0}")", string2);
 	std::wcout << string1.str() << std::endl;
 	std::wcout << string2.str() << std::endl;
 	std::wcout << string3.str() << std::endl;
@@ -42,20 +44,20 @@ void usageSample()
 void updateResourceFile() 
 {
 	//open old resource
-	EditableResource<char, wchar_t> oldResource;
+	EditableResource<wchar_t, wchar_t> oldResource;
 	try {
-		std::ifstream xmlFs("strings.xml");
-		boost::archive::xml_iarchive archive(xmlFs, boost::archive::no_header);
+		std::wifstream xmlFs("strings.xml");
+		boost::archive::xml_wiarchive archive(xmlFs, boost::archive::no_header);
 		archive >> boost::serialization::make_nvp("strings", oldResource);
 	} catch (std::exception ex) {
 		std::cout << "cannot load old resource: " << ex.what() << std::endl;
 	}
 
 	//create a resource from build log
-	EditableResource<char, wchar_t> newResource;
-	std::ifstream buildLogFile(BOOST_PP_STRINGIZE(BUILD_LOG_FILE));
-	std::string buildLog((std::istreambuf_iterator<char>(buildLogFile)),
-		std::istreambuf_iterator<char>());
+	EditableResource<wchar_t, wchar_t> newResource;
+	std::wifstream buildLogFile(BOOST_PP_STRINGIZE(BUILD_LOG_FILE));
+	std::wstring buildLog((std::istreambuf_iterator<wchar_t>(buildLogFile)),
+		std::istreambuf_iterator<wchar_t>());
 	newResource.addStringsFromCompilerOutput(buildLog);
 
 	//merge resources
@@ -63,14 +65,18 @@ void updateResourceFile()
 
 	//save updated resource
 	{
-		std::ofstream xmlFs("strings.xml");
-		boost::archive::xml_oarchive archive(xmlFs, boost::archive::no_header);
+		std::wofstream xmlFs("strings.xml");
+		boost::archive::xml_woarchive archive(xmlFs, boost::archive::no_header);
 		archive << boost::serialization::make_nvp("strings", oldResource);
 	}
 }
 
 int main()
 {
+	std::wcout << "User-preferred locale setting is " << std::locale("").name().c_str() << '\n';
+	std::locale::global(std::locale(""));
+	std::wcout.imbue(std::locale());
+
 	std::cout << R"(1 - update strings resource
 2 - run usage sample
 )";
